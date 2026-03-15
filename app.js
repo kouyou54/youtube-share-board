15bcfedcba6b954aec03df9da98a6b14f055d126
kouyou7512630 <kouyou7512630@gmail.com>
2026-03-15 16:12:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 import {
-
 getFirestore,
 collection,
 addDoc,
 deleteDoc,
 doc,
 onSnapshot
-
 }
-
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBONAWg79Un6Tag0vPP0PB0UiqJLL6KvtM",
@@ -23,11 +19,12 @@ const firebaseConfig = {
   appId: "1:972674645025:web:468e8a52a964e4a53e3760"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const videosRef = collection(db,"videos");
+
+let currentDate = null;
 
 
 window.addVideo = async function(){
@@ -44,46 +41,37 @@ return;
 }
 
 await addDoc(videosRef,{
-
 url:url,
 comment:comment,
 date:date
-
 });
 
 document.getElementById("url").value="";
 document.getElementById("comment").value="";
-
 }
-
-
-let currentDate="";
 
 
 function renderDates(snapshot){
 
-const dates=new Set();
+const dates = new Set();
 
 snapshot.forEach(doc=>{
-
 dates.add(doc.data().date);
-
 });
 
-const list=document.getElementById("dateList");
+const list = document.getElementById("dateList");
 
 list.innerHTML="";
 
 dates.forEach(date=>{
 
-const div=document.createElement("div");
+const div = document.createElement("div");
 
 div.className="dateItem";
 
 div.innerText=date;
 
 div.onclick=()=>{
-
 currentDate=date;
 renderVideos(snapshot);
 };
@@ -97,10 +85,6 @@ list.appendChild(div);
 
 function renderVideos(snapshot){
 
-if(!currentDate) return;
-
-document.getElementById("selectedDate").innerText="📅 "+currentDate+" の動画";
-
 const list=document.getElementById("videoList");
 
 list.innerHTML="";
@@ -108,10 +92,9 @@ list.innerHTML="";
 snapshot.forEach(docSnap=>{
 
 const data=docSnap.data();
-
-if(data.date!==currentDate) return;
-
 const id=docSnap.id;
+
+if(currentDate && data.date!==currentDate) return;
 
 const div=document.createElement("div");
 
@@ -119,22 +102,16 @@ div.className="videoCard";
 
 div.innerHTML=`
 
+<strong>📅 ${data.date}</strong>
+
 <div class="url">
-
-<a href="${data.url}" target="_blank">
-
-${data.url}
-
-</a>
-
+<a href="${data.url}" target="_blank">${data.url}</a>
 </div>
 
 <p>${data.comment || ""}</p>
 
 <button class="delete" onclick="deleteVideo('${id}')">
-
 削除
-
 </button>
 
 `;
